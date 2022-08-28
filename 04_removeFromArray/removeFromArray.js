d25b337fb839f3cf48dbc0c998db05a452297a6a
Ghassen <dhiebghassen@gmail.com>
2022-08-28 15:50:18 +0100

    Remove element/elements from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function (array) {
 
+    for (let i=1 ; i< arguments.length ; i++){
+
+        let index=array.indexOf(arguments[i]) ;
+
+        if (index!=-1){
+
+            array.splice(index, 1);
+        }
+        else continue ;
+    }
+    
+    
+    return array;
 };
 
 // Do not edit below this line
