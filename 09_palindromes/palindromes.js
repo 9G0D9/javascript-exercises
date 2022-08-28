3d28f8825981d529c8d76a96f09acb186d631005
Ghassen <dhiebghassen@gmail.com>
2022-08-29 00:55:13 +0100

    Create palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+
+    let reversedString='' ;
+    let normalString='';
+    let alphabet=['a','b' , 'c' , 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+    for (let i=0 ; i<str.length ; i++){
+        if (alphabet.indexOf(str[i].toLowerCase())>-1){
+            normalString+=str[i].toLowerCase();
+        }
+    }
+
+    for (let i=(normalString.length -1) ; i>=0 ; i--){
+        reversedString+=normalString[i] ;
+    }
+
+    return normalString === reversedString ;
 
 };
 
