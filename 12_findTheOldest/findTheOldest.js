2cf19a3bb0b18adb0e3d8009506eec2ae2c12b6e
Ghassen <dhiebghassen@gmail.com>
2022-08-29 18:44:31 +0100

    Find the oldest person from a list of objects

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,34 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
 
-};
+    let ages = [];
+    let oldest = { name: '', age: 0 };
+
+    
+
+
+    for (let i = 0; i < array.length; i++) {
+        ages[i] = {};
+        ages[i].name = array[i].name
+
+        if (array[i].yearOfDeath === undefined) {
+            
+            ages[i].age = 2022 - array[i].yearOfBirth;
+        }
+        else{
+            ages[i].age = array[i].yearOfDeath - array[i].yearOfBirth;}
+
+    }
+    oldest = { name: '', age: 0 };
+    for (let j = 0; j < ages.length; j++) {
+        if (ages[j].age > oldest.age) {
+            oldest = ages[j];
+        }
+    }
+
+    return oldest;
+
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
