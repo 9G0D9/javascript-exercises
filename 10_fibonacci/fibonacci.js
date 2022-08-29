818e26a29d62a901ee512a107e14f9bf534582e2
Ghassen <dhiebghassen@gmail.com>
2022-08-29 12:34:56 +0100

    create the fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+
+    let fibArray=[1,1] ;
+
+    if (member<=0) return 'OOPS'
+
+    if (member>2){
+        for (let i=2; i<member ; i++){
+            fibArray[i]=fibArray[i-1]+fibArray[i-2];
+        }
+        
+    }
+    return fibArray[member-1]
 
 };
 
