c3bd82fd97f682e85c3f39f5629cb7baa9d366cb
Ghassen <dhiebghassen@gmail.com>
2022-08-29 19:24:48 +0100

    create caesar encryption function

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,4 +1,38 @@
-const caesar = function() {
+const caesar = function (str, numb) {
+
+    let encodedStr = '';
+    let ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    
+    if (numb<0){
+        numb=26+numb ;
+    }
+
+    if (numb > 26) {
+        numb = numb % 26;
+    }
+
+    for (let i = 0; i < str.length; i++) {
+
+        if (ALPHABET.indexOf(str[i]) !== -1) {
+
+            let index = (ALPHABET.indexOf(str[i]) + numb) % 26;
+            encodedStr += ALPHABET[index];
+        }
+
+        else if (alphabet.indexOf(str[i]) !== -1) {
+
+            let index = (alphabet.indexOf(str[i]) + numb) % 26;
+            encodedStr += alphabet[index];
+        }
+
+        else {
+            encodedStr += str[i]
+        }
+
+    }
+
+    return encodedStr;
 
 };
 
